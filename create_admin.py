1eee5ec5b05dba2832c1d3a351de662e8c78693b
alexfedorov81-cloud <alex.fedorov81@gmail.com>
2025-11-18 17:05:01 +0700

    Update create_admin.py

--- create_admin.py
+++ create_admin.py
@@ -2,7 +2,7 @@ import os
 import django
 from django.contrib.auth import get_user_model
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'colledge_schedule.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'colledge_schedule-1.settings')
 django.setup()
 
 User = get_user_model()
